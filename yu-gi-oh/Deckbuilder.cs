986b3607c5ad89e59c87968c8d4f649da873c2c5
ile1337 <ilija.jakimovski@students.finki.ukim.mk>
2021-07-27 01:01:35 +0200

    Merged with YGO-36 Added restrictions for next page Cleaned up code Parameterized construct methods for easier usage later Cleaned up memory leaks in DeckBuilder

--- yu-gi-oh/Deckbuilder.cs
+++ yu-gi-oh/Deckbuilder.cs
@@ -13,6 +13,7 @@ namespace yu_gi_oh
     public partial class Deckbuilder : Form
     {
         public int currentPage { get; set; } = 0;
+        public int maxPage { get; set; } = int.MaxValue;
 
         public BindingList<CardDto> cards = new();
 
@@ -22,104 +23,89 @@ namespace yu_gi_oh
         public Deckbuilder()
         {
             InitializeComponent();
-            lbDeckCards.AllowDrop = true;
-            dgv1.AutoGenerateColumns = true;
-            dgv1.DataSource = cards;
-            dgv1.AutoGenerateColumns = false;
             init();
 
         }
 
-        public void ConstructDGV()
+        public void ConstructDGV(DataGridView dgv, BindingList<CardDto> source)
         {
+            // Base configurations
+            dgv.AllowUserToAddRows = false;
+
+            dgv.AutoGenerateColumns = true;
+            dgv.DataSource = source;
+            dgv.AutoGenerateColumns = false;
+
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            dgv.RowTemplate.Height = 100;
 
             // Disable useless columns
-            dgv1.Columns["id"].Visible = false;
-            dgv1.Columns["cardId"].Visible = false;
-            dgv1.Columns["description"].Visible = false;
-            dgv1.Columns["subType"].Visible = false;
+            dgv.Columns["id"].Visible = false;
+            dgv.Columns["cardId"].Visible = false;
+            dgv.Columns["description"].Visible = false;
+            dgv.Columns["subType"].Visible = false;
 
             int idx = 0;
-            foreach(KeyValuePair<string, string> item in dgvNaming)
+            foreach (KeyValuePair<string, string> item in dgvNaming)
             {
-                DataGridViewColumn col = dgv1.Columns[item.Key];
+                DataGridViewColumn col = dgv.Columns[item.Key];
                 col.HeaderText = item.Value;
                 col.DisplayIndex = idx++;
                 col.ReadOnly = true;
+
+                // Set images as Stretch
+                if (item.Key == "img")
+                {
+                    (col as DataGridViewImageColumn).ImageLayout = DataGridViewImageCellLayout.Stretch;
+                    col.Width = 60;
+                }
             }
         }
 
         public void init()
         {
-            System.Data.DataTable table = new System.Data.DataTable();
-            table.Columns.Add("Name", typeof(string));
-            table.Columns.Add("Type", typeof(string));
-           // DataGridViewImageColumn imageCol = new DataGridViewImageColumn();
-            //imageCol.HeaderText = "Image";
-            table.Columns.Add("Image", typeof(Image)); 
-            ConstructDGV();
-            
-            //dataGridView1.Columns.Add(imageCol);
-           
-            loadingPB.Visible = true;
-            LoadDataTable(table, Middleware.Models.Meta.Direction.FORWARDS);
-            dgv1.DataSource = table;
-        }
-            dgv1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
-            dgv1.RowTemplate.Height = 100;
-            
-            //dgv1.Defa
-            
-                for (int i = 0; i < dgv1.Columns.Count; i++) { 
-                if (dgv1.Columns[i] is DataGridViewImageColumn)
-                {
-                    ((DataGridViewImageColumn)dgv1.Columns[i]).ImageLayout = DataGridViewImageCellLayout.Stretch;
-                    break;
-                }
+            ConstructDGV(dgv1, cards);
 
-            }
+            loadingPB.Visible = true;
+            LoadDataTable(Middleware.Models.Meta.Direction.FORWARDS);
         }
+
 
         private void LoadDataTable(Middleware.Models.Meta.Direction direction)
-            if  (currentPage < 1)
+        {
+            currentPage += (int)direction;
+
+            if (currentPage < 1)
             {
                 currentPage = 1;
             }
 
             Middleware.Controllers.CardController.GetAllCardDtosShortAsync(new CardDto(), currentPage).ContinueWith(t =>
             {
-                foreach (CardDto card in t.Result.content)
-                {
-            {
-                Invoke((MethodInvoker)(() => {
-                    foreach(CardDto card in cards)
-                    {
-                        card.img.Dispose();
-                    }
-                    cards.Clear();
-                    }));
-                foreach (CardDto card in t.Result.content)
+                Invoke((MethodInvoker)(() => CleanCards()));
+
+                Middleware.Models.Meta.PageResponse<CardDto> page = t.Result;
+                maxPage = page.totalPages;
+
+                foreach (CardDto card in page.content)
                 {
                     card.img = Middleware.Controllers.YGOController.GetImage(card.cardId);
                     Invoke((MethodInvoker)(() => cards.Add(card)));
                 }
-                Invoke((MethodInvoker)(() => {
-                    loadingPB.Visible = false;
-                    }));
 
-            for (int i = 0; i < dgv1.Columns.Count; i++)
-            {
-                if (dgv1.Columns[i] is DataGridViewImageColumn)
-                {
-                    dgv1.Columns[i].Width = 60;
-
-                }
-                
-
-            }
+                Invoke((MethodInvoker)(() => loadingPB.Visible = false));
+            });
         }
 
-
+        private void CleanCards()
+        {
+            foreach (CardDto card in cards)
+            {
+                if(card.img != null) card.img.Dispose();
+            }
+            cards.Clear();
+        }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
@@ -151,55 +137,56 @@ namespace yu_gi_oh
         private bool isMorethan3(CardDto c)
         {
             int ctr = 0;
-            if (lbDeckCards.Items.Count > 0)
-            {
-                foreach (CardDto card in lbDeckCards.Items)
-                {
-                    if (card == c)
-                    {
-                        ctr++;
-                    }
-                }
+            //if (lbDeckCards.Items.Count > 0)
+            //{
+            //    foreach (CardDto card in lbDeckCards.Items)
+            //    {
+            //        if (card == c)
+            //        {
+            //            ctr++;
+            //        }
+            //    }
 
-            }
+            //}
             if (ctr >= 3) return true;
             else return false;
         }
         private bool maxCards()
         {
-            return lbDeckCards.Items.Count > 30;
+            //return lbDeckCards.Items.Count > 30;
+            return false;
         }
 
         private void btnRemoveFromDeck_Click(object sender, EventArgs e)
         {
-            if (lbDeckCards.SelectedIndex != -1)
-            {
-                lbDeckCards.Items.Remove(lbDeckCards.SelectedItem);
-            }
+            //if (lbDeckCards.SelectedIndex != -1)
+            //{
+            //    lbDeckCards.Items.Remove(lbDeckCards.SelectedItem);
+            //}
         }
 
         private void btnSaveDeck_Click(object sender, EventArgs e)
         {
-            if (lbDeckCards.Items.Count > 0)
-            {
+            //if (lbDeckCards.Items.Count > 0)
+            //{
 
-                foreach (CardDto card in lbDeckCards.Items)
-                {
-                    deck.cards.Add(card);
-                }
+            //    foreach (CardDto card in lbDeckCards.Items)
+            //    {
+            //        deck.cards.Add(card);
+            //    }
 
-                SaveFileDialog sfd = new SaveFileDialog();
-                if (sfd.ShowDialog() == DialogResult.OK)
-                {
-                    saveAsync(sfd.FileName);
-                }
-                else
-                {
-                    MessageBox.Show("You need to enter a name for the new deck !", "ERROR");
-                    return;
-                }
+            //    SaveFileDialog sfd = new SaveFileDialog();
+            //    if (sfd.ShowDialog() == DialogResult.OK)
+            //    {
+            //        saveAsync(sfd.FileName);
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("You need to enter a name for the new deck !", "ERROR");
+            //        return;
+            //    }
 
-            }
+            //}
         }
 
         private async void saveAsync(string s)
@@ -214,7 +201,7 @@ namespace yu_gi_oh
         {
             if (MessageBox.Show("Are You sure you want to remove all cards from your deck ?", "Are you sure ?", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
-                lbDeckCards.Items.Clear();
+                //lbDeckCards.Items.Clear();
             }
             else
             {
@@ -229,7 +216,7 @@ namespace yu_gi_oh
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                lbDeckCards.Items.Clear();
+                //lbDeckCards.Items.Clear();
                 openAsync(ofd.FileName);
             }
         }
@@ -240,7 +227,7 @@ namespace yu_gi_oh
                 Deck deck = await System.Text.Json.JsonSerializer.DeserializeAsync<Deck>(fs);
                 foreach (CardDto card in deck.cards)
                 {
-                    lbDeckCards.Items.Add(card);
+                    //lbDeckCards.Items.Add(card);
                 }
 
             }
@@ -253,7 +240,7 @@ namespace yu_gi_oh
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+
         }
 
         private void Deckbuilder_Load(object sender, EventArgs e)
@@ -263,41 +250,21 @@ namespace yu_gi_oh
 
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Data.DataTable table = new System.Data.DataTable();
-            table.Columns.Add("Name", typeof(string));
-            table.Columns.Add("Type", typeof(string));
-            }
-            else
-            {
-                button1.Enabled = true;
-            }
-            System.Data.DataTable table = new System.Data.DataTable();
-            table.Columns.Add("Name", typeof(string));
-            table.Columns.Add("Type", typeof(string));
-            table.Columns.Add("Image", typeof(Image));
             loadingPB.Visible = true;
-            LoadDataTable(table, Middleware.Models.Meta.Direction.FORWARDS);
-            dgv1.DataSource = table;
+            LoadDataTable(Middleware.Models.Meta.Direction.FORWARDS);
+            button1.Enabled = true;
+            if (currentPage >= maxPage) button2.Enabled = false;
         }
 
-            //if (currentPage == 1) button1.Enabled = false;
 
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Data.DataTable table = new System.Data.DataTable();
-            table.Columns.Add("Name", typeof(string));
-            table.Columns.Add("Type", typeof(string));
-                table.Columns.Add("Image", typeof(Image));
-                loadingPB.Visible = true;
-                LoadDataTable(table, Middleware.Models.Meta.Direction.BACKWARDS);
-                dgv1.DataSource = table;
-                button2.Enabled = true;
+            loadingPB.Visible = true;
+            LoadDataTable(Middleware.Models.Meta.Direction.BACKWARDS);
+            button2.Enabled = true;
+            if (currentPage <= 1) button1.Enabled = false;
+        }
 
-                if(currentPage == 1) button1.Enabled = false;
-            } 
-                
     }
-
 }
